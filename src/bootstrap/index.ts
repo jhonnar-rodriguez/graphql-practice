7ece9b2fb49db50157e1ece8c4fc5b2879a2e5ce
Jhonnar Rodriguez <jhonnar.rodriguez@gmail.com>
2021-12-30 12:07:27 -0500

    feat: add try-catch while trying to connect to the database and if something goes wrong then the start process will be stopped.

--- src/bootstrap/index.ts
+++ src/bootstrap/index.ts
@@ -6,19 +6,26 @@ import databaseConnect from '../config/typeorm';
 const { APP_PORT, APP_GRAPHQL_PATH = 'api' } = process.env;
 
 const startServer = async () => {
-  const app = express();
+  try {
+    await databaseConnect();
 
-  app.use(routes);
+    const app = express();
 
-  setUpApolloServer(app, APP_GRAPHQL_PATH);
+    app.use(routes);
 
-  app.listen(APP_PORT, () => {
-    console.log(`🚀 Server ready at http://localhost:${APP_PORT}`);
-    console.log(`🚀 GraphQL ready at http://localhost:${APP_PORT}${APP_GRAPHQL_PATH}`);
-  });
+    setUpApolloServer(app, APP_GRAPHQL_PATH);
+
+    app.listen(APP_PORT, () => {
+      console.log(`🚀 Server ready at http://localhost:${APP_PORT}`);
+      console.log(`🚀 GraphQL ready at http://localhost:${APP_PORT}${APP_GRAPHQL_PATH}`);
+    });
+  } catch ({ message }) {
+    console.error('Something went wrong starting the server, please try again. Details: ', message);
+
+    process.exit(1);
+  }
 };
 
-databaseConnect();
 startServer();
 
 export default startServer;
